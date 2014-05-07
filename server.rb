c3ae560bc97e4a3f1a2728a5fb903cd5def86c2c
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-07 09:14:03 +0100

    deleted files removed

--- server.rb
+++ server.rb
@@ -1,11 +1,18 @@
+require 'sinatra'
+require 'link'
+require "data_mapper"
+
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, "postgres://localhost/book_manager_#{env}")
 
-require './lib/link'
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
 
-dbtype://user:password@hostname:port/databasename
+# dbtype://user:password@hostname:port/databasename
+
+get '/' do
+  erb :index
+end
