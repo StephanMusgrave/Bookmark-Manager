39bb2692d5874feeba620ff420d9a2605eb6c62f
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-09 08:39:39 +0100

    Friday

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -13,21 +13,20 @@ feature "User adds a new link" do
   end
 
   scenario "with a few tags" do
-    visit "/"
-    add_link("http://makersacademy.com/",
-              "Makers Academy", 
-              ['education','ruby'])
+    visit '/'
+    add_link("http://www.makersacademy.com/", "Makers Academy", ['education', 'ruby'])
+     
     link = Link.first
-    expect(link.tags).to include("education")
-    expect(link.tags).to include("ruby")
+    expect(link.tags.map(&:text)).to include("education")
+    expect(link.tags.map(&:text)).to include("ruby")
   end
 
   def add_link(url, title, tags=[])
     within ('#new-link') do
-      fill_in 'url', :with => url
+      fill_in 'url',   :with => url
       fill_in 'title', :with => title
-      fill_in 'tags', :with => tags.join(' ')
-
+      # the tags will be space separated
+      fill_in 'tags',  :with => tags.join(' ')
       click_button 'Add link'
     end
   end
